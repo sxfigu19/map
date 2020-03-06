3bf7f8ceada359adf97050d36650804ca7890f31
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-03-06 13:44:11 -0500

    update

--- main.py
+++ main.py
@@ -2,26 +2,43 @@ import pyglet # import the library
 win= pyglet.window.Window() # create the window
 
 # Create a sprite
-tile= pyglet.image.load('images/assets/ground/grey.png')
-spr = pyglet.sprite.Sprite(tile)
+tilegrey= pyglet.image.load('images/assets/ground/grey.png')
+spr1 = pyglet.sprite.Sprite(tilegrey)
+tileblue= pyglet.image.load('images/assets/ground/blue.png')
+spr2 = pyglet.sprite.Sprite(tileblue)
+tilewhite= pyglet.image.load('images/assets/ground/white.png')
+spr3 = pyglet.sprite.Sprite(tilewhite)
 
 def update(dt):
   pass
 
-sprites = []
-for i in range(4):
-    #sprites.append(pyglet.sprite.Sprite(tile, x = i * 48, y = 0))
-    for j in range(4):
-        sprites.append(pyglet.sprite.Sprite(tile, x = i * 48, y = j * 50))
+grey = []
+for i in range(14):
+    for j in range(10):
+        grey.append(pyglet.sprite.Sprite(tilegrey, x = i * 48, y = j * 50))
+
+blue = []
+for i in range(14):
+    for j in range(5):
+        blue.append(pyglet.sprite.Sprite(tileblue, x = i * 48, y = j * 150))
+
+white = []
+for i in range(14):
+    for j in range(5):
+        white.append(pyglet.sprite.Sprite(tilewhite, x = i * 144, y = j * 100))
 
 # Start the event loop
 @win.event
 def on_draw():
     win.clear()
-    for i in range(16):
-        sprites[i].draw()
-    # for j in range(4):
-    #     sprites[j].draw()
+    for i in range(140):
+        grey[i].draw()
+    for i in range(70):
+        blue[i].draw()
+    for i in range(70):
+        white[i].draw()
+    
+
 
 pyglet.clock.schedule(update) 
 pyglet.app.run()
